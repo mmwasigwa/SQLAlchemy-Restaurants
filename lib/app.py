462db8898c2ae0cf5abd204b194dcfda07ea6208
Mwasigwa2 <gathirimu.mwasigwa@student.moringaschool.com>
2023-09-04 10:48:36 +0300

    Done

--- lib/app.py
+++ lib/app.py
@@ -1,2 +1,9 @@
-def Reviews (self, reviews)
-    if 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Customer, Restaurant, Review
+
+engine = create_engine('your_database_url_here')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Test your methods here
